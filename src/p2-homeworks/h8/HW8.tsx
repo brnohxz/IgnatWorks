14010ea3c7393b81ea39bfe85c37ab0f29cc65d8
BRNOHXZ <gkvznvtj@icloud.com>
2021-09-21 00:36:17 +0300

    HW9 done

--- src/p2-homeworks/h8/HW8.tsx
+++ src/p2-homeworks/h8/HW8.tsx
@@ -2,6 +2,8 @@ import React, {useState} from 'react'
 import {checkAC, homeWorkReducer, sortUpAC} from './bll/homeWorkReducer'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
 import s from './HW8.module.css'
+import {Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@material-ui/core";
+import {makeStyles} from "@material-ui/core/styles";
 // export type UserType =
 export type initialPeopleObject = {
     _id: number
@@ -17,31 +19,59 @@ const initialPeople = [
     {_id: 5, name: 'Ирина', age: 55},
 ]
 
+const useStyles = makeStyles({
+    table: {
+        minWidth: 350,
+    },
+});
+
 function HW8() {
     const [people, setPeople] = useState<initialPeopleObject[]>(initialPeople) // need to fix any
 
     // need to fix any
-    const finalPeople = people.map((p: initialPeopleObject) => (
-        <div key={p._id} className={s.tableItem}>
-            <div>
-                {p.name}
-            </div>
-            <div>
-                {p.age}
-            </div>
-        </div>
-    ))
+    const classes = useStyles();
+    // const finalPeople = people.map((p: initialPeopleObject) => (
+    //     <div key={p._id} className={s.tableItem}>
+    //         <div>
+    //             {p.name}
+    //         </div>
+    //         <div>
+    //             {p.age}
+    //         </div>
+    //     </div>
+    // ))
 
     const sortAdult = () => setPeople(homeWorkReducer(initialPeople, checkAC(18)))
     const sortUP = () => setPeople(homeWorkReducer(initialPeople,sortUpAC('up')))
     const sortDown = () => setPeople(homeWorkReducer(initialPeople,sortUpAC('down')))
     return (
-        <div style={{minWidth:250}}>
+        <div style={{display: 'flex',flexDirection: 'column', alignItems: 'center'}}>
             <hr/>
             homeworks 8
 
             {/*should work (должно работать)*/}
-            <div className={s.tableWrap}>{finalPeople}</div>
+            {/*<div className={s.tableWrap}>{finalPeople}</div>*/}
+
+            <TableContainer component={Paper} style={{maxWidth:350,margin:30}}>
+                <Table className={classes.table} size="small" aria-label="a dense table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Name</TableCell>
+                            <TableCell align="right">Age</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {people.map((p) => (
+                            <TableRow key={p._id}>
+                                <TableCell component="th" scope="row">
+                                    {p.name}
+                                </TableCell>
+                                <TableCell align="right">{p.age}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
             <div className={s.buttonGroup}>
             <div><SuperButton onClick={sortAdult}>sort adult</SuperButton></div>
             <div><SuperButton onClick={sortUP}>sort up</SuperButton></div>
@@ -51,6 +81,10 @@ function HW8() {
             {/*для личного творчества, могу проверить*/}
             {/*<AlternativePeople/>*/}
             <hr/>
+
+
+
+
         </div>
     )
 }
--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import {Typography} from "@material-ui/core";
 
 function Clock() {
     const [timerId, setTimerId] = useState<number>(0)
@@ -8,44 +9,54 @@ function Clock() {
 
     const stop = () => {
         // stop
+        clearInterval(timerId)
     }
     const start = () => {
         stop()
         const id: number = window.setInterval(() => {
             // setDate
+            setDate(new Date())
         }, 1000)
         setTimerId(id)
     }
 
     const onMouseEnter = () => {
-        // show
+        setShow(true)
     }
     const onMouseLeave = () => {
-        // close
+        setShow(false)
     }
 
-    const stringTime = 'Time' // fix with date
-    const stringDate = 'Date' // fix with date
+    const hours = date ? (date.getHours() < 10 ? '0' + date.getHours() : date.getHours()) + ':' : '' // fix with date
+    const minutes = date ? (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()) + ':' : '' // fix with date
+    const seconds = date ? (date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds()) : '' // fix with date
+    const stringDate = date ? date.toLocaleDateString() : '' // fix with date
 
     return (
         <div>
-            <div
-                onMouseEnter={onMouseEnter}
-                onMouseLeave={onMouseLeave}
-            >
-                {stringTime}
+            <div onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
+                <Typography variant="h2" gutterBottom component={'span'}>
+                    <Typography variant="h2" gutterBottom component={'span'}>
+                        {hours}
+                    </Typography>
+                    <Typography variant="h2" gutterBottom component={'span'}>
+                        {minutes}
+                    </Typography>
+                    <Typography variant="h2" gutterBottom component={'span'}>
+                        {seconds}
+                    </Typography>
+                </Typography>
             </div>
-
-            {show && (
-                <div>
-                    {stringDate}
-                </div>
-            )}
+            {show && (<div>
+                <Typography>
+                {stringDate}
+                </Typography>
+            </div>)}
 
             <SuperButton onClick={start}>start</SuperButton>
-            <SuperButton onClick={stop}>stop</SuperButton>
-
+            <SuperButton onClick={stop} red>stop</SuperButton>
         </div>
+
     )
 }
 
