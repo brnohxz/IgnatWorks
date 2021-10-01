fd510c4efc8cd3b28bb0996f1058c7d9784d01dd
vladimirmartynyuk <martynyukvm@mail.ru>
2021-10-01 16:54:32 +0300

    HW10 works. Redux fixed

--- src/p2-homeworks/h10/HW10.tsx
+++ src/p2-homeworks/h10/HW10.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import {useDispatch, useSelector} from "react-redux";
+import {AppStoreType} from "./bll/store";
+import {loadingAC} from "./bll/loadingReducer";
+import LinearProgress from '@material-ui/core/LinearProgress';
 
 function HW10() {
     // useSelector, useDispatch
-    const loading = false
+    const loading = useSelector<AppStoreType>(state=>state.loading.isLoading)
 
+    const dispatch = useDispatch()
     const setLoading = () => {
+        dispatch(loadingAC(true))
+        setTimeout(()=>{
+            dispatch(loadingAC(false))
+        },3000)
         // dispatch
         // setTimeout
         console.log('loading...')
@@ -19,7 +28,7 @@ function HW10() {
             {/*should work (должно работать)*/}
             {loading
                 ? (
-                    <div>крутилка...</div>
+                    <LinearProgress />
                 ) : (
                     <div>
                         <SuperButton onClick={setLoading}>set loading...</SuperButton>
--- src/p2-homeworks/h10/bll/loadingReducer.ts
+++ src/p2-homeworks/h10/bll/loadingReducer.ts
@@ -1,14 +1,24 @@
-const initState = {
-
+const initState:initState = {
+    isLoading:false
+}
+export type initState = {
+    isLoading : boolean
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state = initState, action: MainACType): initState => { // fix any
     switch (action.type) {
-        case '': {
-            return state
+        case 'LOAD_STATUS': {
+            let copyState = {...state}
+            copyState.isLoading = action.loadStatus
+            return copyState
         }
         default: return state
     }
 }
 
-export const loadingAC = (): any => {} // fix any
+type MainACType = LoadingAC
+type LoadingAC = {type: 'LOAD_STATUS'
+    loadStatus:boolean}
+export const loadingAC = (loadStatus:boolean): LoadingAC => {
+    return {type:'LOAD_STATUS',loadStatus}
+} // fix any
