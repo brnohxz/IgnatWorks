64f3beafaea127133ac0448fa92b7918d43dc422
vladimirmartynyuk <martynyukvm@mail.ru>
2021-10-01 17:56:36 +0300

    index fixed

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import {Provider} from "react-redux";
 import store from "./p2-homeworks/h10/bll/store";
 
 ReactDOM.render(
-    <React.StrictMode>ß
+    <React.StrictMode>
         <Provider store={store}><App/></Provider>
     </React.StrictMode>,
     document.getElementById("root")
